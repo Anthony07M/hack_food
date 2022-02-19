e09a2619e7f7c5acbcb49a91b840fbf5026ffc2c
Anthony Freitas <anthonyfreitas2015@gmail.com>
2022-02-19 19:46:14 -0400

    fix: build

---
+++ src/migration/1645313662687-agoravai.ts
@@ -0,0 +1,26 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class agoravai1645313662687 implements MigrationInterface {
+    name = 'agoravai1645313662687'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "food" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "nutritionalInfo" character varying NOT NULL, "type" character varying NOT NULL, CONSTRAINT "PK_26d12de4b6576ff08d30c281837" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "recipes" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "author" character varying NOT NULL, "type" character varying NOT NULL, CONSTRAINT "PK_8f09680a51bf3669c1598a21682" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "recipes_foods_food" ("recipesId" uuid NOT NULL, "foodId" uuid NOT NULL, CONSTRAINT "PK_3e5ecbc0bcc435ee057d0141e1d" PRIMARY KEY ("recipesId", "foodId"))`);
+        await queryRunner.query(`CREATE INDEX "IDX_0b750b13e3a4b11ed6e8c06d22" ON "recipes_foods_food" ("recipesId") `);
+        await queryRunner.query(`CREATE INDEX "IDX_f4810a291126712784ea59503e" ON "recipes_foods_food" ("foodId") `);
+        await queryRunner.query(`ALTER TABLE "recipes_foods_food" ADD CONSTRAINT "FK_0b750b13e3a4b11ed6e8c06d222" FOREIGN KEY ("recipesId") REFERENCES "recipes"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
+        await queryRunner.query(`ALTER TABLE "recipes_foods_food" ADD CONSTRAINT "FK_f4810a291126712784ea59503e8" FOREIGN KEY ("foodId") REFERENCES "food"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "recipes_foods_food" DROP CONSTRAINT "FK_f4810a291126712784ea59503e8"`);
+        await queryRunner.query(`ALTER TABLE "recipes_foods_food" DROP CONSTRAINT "FK_0b750b13e3a4b11ed6e8c06d222"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_f4810a291126712784ea59503e"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_0b750b13e3a4b11ed6e8c06d22"`);
+        await queryRunner.query(`DROP TABLE "recipes_foods_food"`);
+        await queryRunner.query(`DROP TABLE "recipes"`);
+        await queryRunner.query(`DROP TABLE "food"`);
+    }
+
+}
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { app } from './app'
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
-
+console.log('==> ', PORT)
 app.listen(PORT, () => {
     console.log(`running server http://localhost:${PORT}`)
 })
